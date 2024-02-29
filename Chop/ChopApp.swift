9cb21afae7ede6fa4b720fe15755c8b5ab7495f3
Marvin Kome <marvin.kome@movinga.com>
2024-02-29 15:48:40 +0100

    Initial Commit

---
+++ Chop/ChopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopApp.swift
+//  Chop
+//
+//  Created by Marvin Kome on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ChopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
